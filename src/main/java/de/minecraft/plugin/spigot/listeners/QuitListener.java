d14f466c33048134ac87a78287a840e057259601
Florian <florian.meyer.2004@gmail.com>
2022-06-16 23:24:40 +0200

    added: QuitListener.java

--- src/main/java/de/minecraft/plugin/spigot/listeners/QuitListener.java
+++ src/main/java/de/minecraft/plugin/spigot/listeners/QuitListener.java
@@ -11,6 +11,7 @@ public class QuitListener implements Listener {
     private PacMan instance = PacMan.getInstance();
     @EventHandler
     public void onQuit(PlayerQuitEvent event){
+
         Player player = event.getPlayer();
         event.setQuitMessage((String)instance.getMessageFile().getValue("World.Quit", player));
     }
